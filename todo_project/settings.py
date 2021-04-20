1e1009e72942fc61bf24581f620bffa5738f7793
anilthapa44600 <anilthapa44600@gmail.com>
2021-04-20 11:56:50 +0545

    password feature added

--- todo_project/settings.py
+++ todo_project/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,8 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '@jp@42pm3eg^o6e7g3tdzn@ho+1xa^3z(zcohx=9=mc)%fa*2t'
-
+SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
@@ -123,3 +123,9 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST= config('EMAIL_HOST')
+EMAIL_HOST_USER= config('EMAIL_HOST_USER')
+EMAIL_PORT= config('EMAIL_PORT', cast = int)
+EMAIL_HOST_PASSWORD= config('EMAIL_HOST_PASSWORD')
+EMAIL_USE_TLS= config('EMAIL_USE_TLS')
--- todo_project/urls.py
+++ todo_project/urls.py
@@ -16,17 +16,21 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 from users import views as users_views
+from django.contrib.auth import views as auth_views
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('todo.urls')),
-    path('login', users_views.login_view, name='login'),
-    path('logout', users_views.logout_view, name='logout'),
-    path('register', users_views.register_view, name='register'),
-    path('profile', users_views.profile_view, name='profile'),
-
+    path('login/', users_views.login_view, name='login'),
+    path('logout/', users_views.logout_view, name='logout'),
+    path('register/', users_views.register_view, name='register'),
+    path('profile/', users_views.profile_view, name='profile'),
+    path('password-reset/', auth_views.PasswordResetView.as_view(template_name='users/password_reset.html'), name='password_reset'),
+    path('password-reset-done/', auth_views.PasswordResetDoneView.as_view(template_name='users/password_reset_done.html'), name='password_reset_done'),
+    path('password-reset-confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='users/password_reset_confirm.html'), name='password_reset_confirm'),
+    path('password-reset-complete/', auth_views.PasswordResetCompleteView.as_view(template_name='users/password_reset_complete.html'), name='password_reset_complete'),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
